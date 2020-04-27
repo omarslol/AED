ef00877e3202b2c51f36e4f15e669262a3c4109d
Omar <63463675+omarslol@users.noreply.github.com>
2020-04-27 13:24:01 -0300

    Update Adición.cpp

--- 01-Adición/Adición.cpp
+++ 01-Adición/Adición.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int a=0, b=0, r=0;
+    cout<<"******Suma de Numeros Enteros******"<<endl;
+    cout<<"Ingrese un numero entero: ";
+    cin>>a;
+    cout<<"Ingrese otro numero entero: ";
+    cin>>b;
+    r=a+b;
+    cout<<"El resultado de la suma es: "<<r<<endl;
+    //otra forma
+    cout<<"El resultado de la suma es: "<<a+b<<endl;
+    return 0;
+}
