e8088d81f72031f6e6c140988af5cc628615f919
Omar <63463675+omarslol@users.noreply.github.com>
2020-06-09 15:59:34 -0300

    Update IsBisiesto.cpp

--- 03-Bisiesto/IsBisiesto.cpp
+++ 03-Bisiesto/IsBisiesto.cpp
@@ -1,1 +1,22 @@
+/ *
+IsBisiesto.cpp
+Determinar si el año es bisiesto 
+Omar Rosales
+20200609
+* /
 
+
+
+#include <iostream>
+#include <assert.h>
+
+bool IsBisiesto (int anio);
+
+int main()
+{
+    IsBisiesto(2020);
+}
+
+bool  IsBisiesto(int anio){
+  assert (anio>=1582 && anio%4==0 && (anio%100!=0 || anio%400==0));
+}
