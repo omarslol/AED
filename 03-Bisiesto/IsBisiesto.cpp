eaeab31eb41907e7cd7ec372988af83104d564ae
Omar <63463675+omarslol@users.noreply.github.com>
2020-06-10 00:03:02 -0300

    Update IsBisiesto.cpp

--- 03-Bisiesto/IsBisiesto.cpp
+++ 03-Bisiesto/IsBisiesto.cpp
@@ -5,18 +5,16 @@ Omar Rosales
 20200609
 * /
 
-
-
 #include <iostream>
-#include <assert.h>
+#include <cassert.h>
 
 bool IsBisiesto (int anio);
 
 int main()
 {
-    IsBisiesto(2020);
+    assert (IsBisiesto(2020));
 }
 
 bool  IsBisiesto(int anio){
-  assert (anio>=1582 && anio%4==0 && (anio%100!=0 || anio%400==0));
+  return anio>=1582 && anio%4==0 && (anio%100!=0 || anio%400==0);
 }
